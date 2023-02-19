ff511bf4a9dc9e71bbddd7824257162c67a340bf
Yannis <bakalayann30@gmail.com>
2023-02-18 18:31:09 -0600

    engine maintenace

--- Engine.sh
+++ Engine.sh
@@ -4,14 +4,21 @@
     #Date : 18-Feb-2023
 
 ## ---------- script that helps with engine maintenance -----------------
-
-echo "ERS maintenance" 
-sleep 3 
-yum install net-tools -y 
-sleep 2
-echo "maintenance complete" 
-sleep 3 
-echo "engine in healthy condition" 
+echo "starting Turbbo maintenace"
 sleep 3
-cat /etc/os-release 
+yum install net-tools -y
+sleep 2
+echo "maintenance complete"
+sleep 3
+echo "Turbo in healthy condition"
+sleep 3
+echo "checking control electronics"
+sleep 3
+yum install httpd -y
+sleep 2
+"control electronics scan finished"
+sleep 3
+systemctl enable httpd
+sleep 2
+systemctl status httpd
 
